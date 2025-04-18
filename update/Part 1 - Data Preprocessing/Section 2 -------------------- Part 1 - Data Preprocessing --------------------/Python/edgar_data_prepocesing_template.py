2a1fb31ae7cdbeb3fe7c9f2d3bfd94c94f5f70de
edgardamian <edgar.mora.d@gmail.com>
2025-04-18 13:55:44 -0600

    avance_dia_01

--- update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/edgar_data_prepocesing_template.py
+++ update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/edgar_data_prepocesing_template.py
@@ -5,14 +5,57 @@ Created on Thu Apr 10 22:52:30 2025
 
 @author: edgarmora
 """
-
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# quitar la notación científica de los resultados
+np.set_printoptions(suppress=True)
+
+# setear el directorio de trabajo
+os.chdir("/Users/edgarmora/Documents/GitHub/machinelearning-az/update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python")
+os.getcwd()
+os.listdir()
+
+########################### Importar el dataset ###############################
+# Asegúrate de que 'Data.csv' esté en el mismo directorio o proporciona la ruta completa.
 dataset = pd.read_csv("Data.csv")
 
-x = dataset.iloc[:, :-1].values
+x = dataset.iloc[:,:]
 x
-x = dataset.iloc[:, 3].values
+x = dataset.iloc[:, :-1].values # todo menos la ultima columna
 x
+x = dataset.iloc[:, [0,3]].values# ultima columna
+x
+x = dataset.iloc[:, [0,3]].values# primera y ultima columna
+x
+
+############################## Manejo de datos faltantes ######################
+# Utilizamos SimpleImputer para reemplazar valores nulos con la media de la columna
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')  # Estrategia: reemplazar valores nulos con la media
+
+
+X = dataset.iloc[:, :-1].values  # Seleccionamos todas las columnas excepto la última como variables independientes
+y = dataset.iloc[:, -1].values  # Seleccionamos la última columna como la variable dependiente (objetivo)
+X
+y
+
+##### remplazo de los valores calculados en las columnas 1 y 3 con valores faltantes
+imputer.fit(X[:, 1:3])  # Aplicamos el ajuste a las columnas 1 y 2 (índices 1:3)
+print(imputer.statistics_)
+X[:, 1:3] = imputer.transform(X[:, 1:3])  # Reemplazamos los valores nulos en las columnas seleccionadas
+X
+
+###################### datos factoriales #######################################
+
+
+
+
+
+
+
+
+
+
--- update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/edgar_data_preprocessing_template.R
+++ update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/edgar_data_preprocessing_template.R
@@ -1,1 +1,21 @@
+#setear el directorio de trabajo
+getwd()
+setwd("~/Documents/GitHub/machinelearning-az/update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R")
+dir()
+
+# importar el dataset
 dataset = read.csv("Data.csv")
+
+#completar datos faltantes
+# Taking care of missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Age[is.na(dataset$Age)] <- mean(dataset$Age, na.rm = TRUE)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+dataset$Salary[is.na(dataset$Salary)] <- mean(dataset$Salary, na.rm = TRUE)
+
+###################### datos factoriales #######################################
