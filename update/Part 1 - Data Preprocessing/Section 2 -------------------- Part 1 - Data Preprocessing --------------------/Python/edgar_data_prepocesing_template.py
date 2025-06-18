f35080c6ae88e95980c007ec91f87123083e62a7
edgardamian <edgar.mora.d@gmail.com>
2025-06-17 23:54:20 -0600

    avance2

--- update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/edgar_data_prepocesing_template.py
+++ update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/edgar_data_prepocesing_template.py
@@ -22,33 +22,78 @@ os.listdir()
 # Asegúrate de que 'Data.csv' esté en el mismo directorio o proporciona la ruta completa.
 dataset = pd.read_csv("Data.csv")
 
-x = dataset.iloc[:,:]
+x = dataset.iloc[:,:-1].values
 x
-x = dataset.iloc[:, :-1].values # todo menos la ultima columna
-x
-x = dataset.iloc[:, [0,3]].values# ultima columna
-x
-x = dataset.iloc[:, [0,3]].values# primera y ultima columna
-x
+y = dataset.iloc[:,3].values
+y
+
 
 ############################## 3.Manejo de datos faltantes ######################
 # Utilizamos SimpleImputer para reemplazar valores nulos con la media de la columna
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')  # Estrategia: reemplazar valores nulos con la media
 
-
-X = dataset.iloc[:, :-1].values  # Seleccionamos todas las columnas excepto la última como variables independientes
-y = dataset.iloc[:, -1].values  # Seleccionamos la última columna como la variable dependiente (objetivo)
-X
-y
-
 ##### remplazo de los valores calculados en las columnas 1 y 3 con valores faltantes
-imputer.fit(X[:, 1:3])  # Aplicamos el ajuste a las columnas 1 y 2 (índices 1:3)
+imputer.fit(x[:, 1:3])  # Aplicamos el ajuste a las columnas 1 y 2 (índices 1:3)
 print(imputer.statistics_)
-X[:, 1:3] = imputer.transform(X[:, 1:3])  # Reemplazamos los valores nulos en las columnas seleccionadas
-X
 
-###################### datos categóricos #######################################
+x[:, 1:3] = imputer.transform(x[:, 1:3])  # Reemplazamos los valores nulos en las columnas seleccionadas
+x
+
+###################### 4.datos categóricos #######################################
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+# Aplicamos OneHotEncoder a la primera columna (índice 0)
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))  # Transformamos X y lo convertimos a un array de NumPy
+print(x)
+
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)  # Transformamos las etiquetas categóricas en valores numéricos
+print(y)
+
+###################### 5.dividir datos para entrenamiento #######################################
+from sklearn.model_selection import train_test_split
+# Usamos 80% para entrenamiento y 20% para prueba
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/edgar_data_preprocessing_template.R
+++ update/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/edgar_data_preprocessing_template.R
@@ -8,10 +8,11 @@ dir()
 dataset = read.csv("Data.csv")
 
 ############################## 3.Manejo de datos faltantes ######################
-# Taking care of missing data
+# primera forma de reemplazar datos faltantes en age
 dataset$Age = ifelse(is.na(dataset$Age),
                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Age)
+# segunda forma de reemplazar datos faltantes en age
 dataset$Age[is.na(dataset$Age)] <- mean(dataset$Age, na.rm = TRUE)
 
 dataset$Salary = ifelse(is.na(dataset$Salary),
@@ -20,3 +21,28 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
 dataset$Salary[is.na(dataset$Salary)] <- mean(dataset$Salary, na.rm = TRUE)
 
 ###################### 4.datos categóricos #######################################
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
+
+###################### 5.dividir datos para entrenamiento #######################################
+library(caTools)
+set.seed(123)
+split <- sample.split(dataset$Purchased, SplitRatio = 0.8)
+split
+
+training_set <- subset(dataset, split == TRUE)
+testing_set <- subset(dataset, split == FALSE)
+
+
+
+
+
+
+
+
+
+
